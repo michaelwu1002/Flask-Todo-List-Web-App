178e4f39881f3cb875777e1b5f6e414ea024d309
Michael Wu <Michael Wu>
2021-05-19 14:37:49 -0400

    built add, update, delete, and clear functionalities

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 
@@ -20,12 +20,41 @@ def index():
     print(todo_list)
     return render_template('base.html', todo_list=todo_list)
 
+@app.route('/add', methods=["POST"])
+def add():
+    # add new item to todo list
+    title = request.form.get("title")
+    deadline = request.form.get("deadline")
+    new_todo_deadline = Todo(title=title, complete=False, deadline=deadline)
+    db.session.add(new_todo_deadline)
+    Todo.query.order_by(deadline.desc())
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    # update item in todo list
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    # delete item in todo list
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route('/clear', methods=["POST"])
+def clear():
+    # clear all items from todo list
+    Todo.query.delete()
+    db.session.commit()
+    return redirect(url_for("index"))
+    
+
 if __name__ == "__main__":
     db.create_all()
-
-    curr_date = date.today().strftime("%m/%d/%Y")
-    new_todo_deadline = Todo(title="todo 1", complete=False, deadline=curr_date)
-    db.session.add(new_todo_deadline)
-    db.session.commit()
-
     app.run(debug=True)
